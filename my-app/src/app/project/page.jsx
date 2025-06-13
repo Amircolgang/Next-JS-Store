95c6223a702cc479f3454039f0e57b3ebb9fa597
amircolgang <parham38385747@gmail.com>
2025-06-13 16:48:41 +0330

    Auto commit: changes on 2025-06-13 16:48:40

--- my-app/src/app/project/page.jsx
+++ my-app/src/app/project/page.jsx
@@ -9,31 +9,34 @@ const Project = ({ }) => {
   let item_data = json
   return (
     <>
-      <div>
-        {
-          item_data.map(item => (
-            <div className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px] shadow-2xl">
-              <Image
-                alt="Product Image"
-                width={327}
-                height={142}
-                src={'/images/next.svg'}
-                className="rounded-2xl"
-              />
-              <div className="w-[327px] h-[106px] flex justify-between">
-                <div className="flex flex-col justify-between">
-                  <span>Name : {item.title}</span>
-                  <span>Id : {item.id}</span>
-                  {/* <span>description : {item_data.description}</span> */}
-                  <span>$ {item.price}.00</span>
+      <div className="container m-auto">
+        <div className="flex justify-center gap-[36px] pt-[4rem] flex-wrap ">
+          {
+            item_data.map(item => (
+              <div className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px] shadow-2xl">
+                <Image
+                  alt="Product Image"
+                  width={327}
+                  height={242}
+                  src={'/images/next.svg'}
+                  className="rounded-2xl"
+                />
+                <div className="w-[327px] h-[106px] flex justify-between">
+                  <div className="flex flex-col justify-between">
+                    <span>Name : {item.title}</span>
+                    <span>Id : {item.id}</span>
+                    {/* <span>description : {item_data.description}</span> */}
+                    <span>$ {item.price}.00</span>
+                  </div>
+                  <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
                 </div>
-                <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
               </div>
-            </div>
-          ))
-        }
+            ))
+          }
 
+        </div>
       </div>
+
     </>
   )
 }
