386e31ca858242f8f5b2951780d4d06202a774d6
amircolgang <parham38385747@gmail.com>
2025-06-13 16:40:12 +0330

    Auto commit: changes on 2025-06-13 16:40:10

--- my-app/src/app/project/page.jsx
+++ my-app/src/app/project/page.jsx
@@ -1,15 +1,13 @@
 import Image from "next/image"
-
-const feschData  =  fetch('http://localhost:3030/api/project')
-let item_dataFech = async () => {
-  let data = ''
-  await feschData.then( res =>data = res.json())
-  return data
-} 
+let data = ""
+fetch('http://localhost:3030/api/project')
+.then(res => res.json())
+.then(db => data = db )
+ 
 
 const Project = ({ }) => {
-  const item_data = item_dataFech()
-  console.log(item_data)
+  // const item_data = item_dataFech()
+  console.log(data)
   return (
     <>
     <div>
@@ -23,10 +21,10 @@ const Project = ({ }) => {
         />
         <div className="w-[327px] h-[106px] flex justify-between">
           <div className="flex flex-col justify-between">
-            <span>Name : {item_data.title}</span>
-            <span>Id : {item_data.id}</span>
+            {/* <span>Name : {item_data.title}</span> */}
+            {/* <span>Id : {item_data.id}</span> */}
             {/* <span>description : {item_data.description}</span> */}
-            <span>$ {item_data.price}.00</span>
+            {/* <span>$ {item_data.price}.00</span> */}
           </div>
           <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
         </div>
