0e88e23b262429a910a9a9fbea7379ccafe340cf
amircolgang <parham38385747@gmail.com>
2025-06-13 17:44:16 +0330

    Auto commit: changes on 2025-06-13 17:44:15

--- my-app/src/app/login/singup/page.jsx
+++ my-app/src/app/login/singup/page.jsx
@@ -1,11 +1,91 @@
-const SingUp = ({}) => {
+import NavBar from "@/components/Navabar"
+import Image from "next/image"
+const SingUp = ({ }) => {
   return (
     <>
-        <div>
-            <span>
-                SingUp
-            </span>
+      <div className="container m-auto">
+        <NavBar />
+        <div className="flex items-center justify-center pt-[4.5rem]">
+          <div className="w-fit  bg-[#fff] rounded-2xl shadow-2xl flex">
+            <div className="w-[540px] h-[650px]  flex items-center justify-center">
+              <div className="w-[311px] flex flex-col gap-[1.5rem]">
+                <div className="flex items-center justify-center w-[338px] h-[89px]">
+                  <div className="text-center flex flex-col">
+                    <span className="text-[34px] ">
+                      Sing Up  Page
+                    </span >
+                    <span className="text-[14px]">
+                      Welcome back! Please enter your details.
+                    </span>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-[.5rem]">
+                  <div className="flex items-start flex-col gap-[0.25rem]">
+                    <span>
+                      Name
+                    </span>
+                    <input className="w-[313.97px] h-[41px] rounded-[12px] pl-[0.5rem] shadow-2xl bg-[#ffffff]" type="text" placeholder="Enter your name" />
+                  </div>
+                  <div className="flex items-start flex-col gap-[0.25rem]">
+                    <span>
+                      Email
+                    </span>
+                    <input className="w-[313.97px] h-[41px] rounded-[12px] pl-[0.5rem] shadow-2xl bg-[#ffffff]" type="text" placeholder="Enter your email" />
+                  </div>
+                  <div className="flex items-start flex-col gap-[0.25rem]">
+                    <span>
+                      Email
+                    </span>
+                    <input className="w-[313.97px] h-[41px] rounded-[12px] pl-[0.5rem] shadow-2xl bg-[#ffffff]" type="text" placeholder="Enter your email" />
+                  </div>
+                  <div className="flex flex-col gap-[.5rem]">
+                    <span>
+                      Password
+                    </span>
+                    <input className="w-[313.97px] h-[41px] rounded-[12px] pl-[0.5rem] shadow-2xl" type="text" placeholder="**********" />
+                  </div>
+                </div>
+                <div className="felx flex-col ">
+                  <div className="flex justify-between">
+                    <div>
+                      <input type="checkbox" name="" id="" />
+                      <span>Remember me</span>
+                    </div>
+                    <div>
+                      Forgot password
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-[.5rem] pt-[1.5rem]">
+                    <button className="w-[313.97px] h-[41px] rounded-[12px] pl-[0.5rem] bg-[red] shadow-2xl">
+                      Sing Up
+                    </button>
+                    <div className="w-[313.97px] h-[41px] rounded-[12px] pl-[0.5rem] border-zinc-600 border-[1px] flex items-center justify-center shadow-2xl">
+                      <span >
+                        Sign Up with Google
+                      </span>
+                    </div>
+                  </div>
+                  <div className="pt-[1.5rem]">
+                    <span>Don’t have an account? </span>
+                    <span className="text-[blue]">Sign up fo free!</span>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <div>
+              <Image
+                alt="singin"
+                width={559}
+                height={777}
+                src={'/images/Singin.png'}
+              />
+            </div>
+          </div>
         </div>
+
+      </div>
+
     </>
   )
 }
