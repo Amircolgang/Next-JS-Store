c53341563f4e57b2087de0db5de40a8a8dd42e5d
amircolgang <parham38385747@gmail.com>
2025-06-13 16:52:55 +0330

    Auto commit: changes on 2025-06-13 16:52:55

--- my-app/src/app/project/page.jsx
+++ my-app/src/app/project/page.jsx
@@ -1,19 +1,21 @@
 import Image from "next/image"
+import Link from "next/link";
 let data = ""
 let db = await fetch('http://localhost:3030/api/project')
 const json = await db.json();
-
+import Header from "@/components/header";
 
 const Project = ({ }) => {
   // const item_data = item_dataFech()
   let item_data = json
   return (
     <>
+      <Header/>
       <div className="container m-auto">
         <div className="flex justify-center gap-[36px] pt-[4rem] flex-wrap ">
           {
-            item_data.map(item => (
-              <div className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px] shadow-2xl">
+            item_data.map((item , index) => (
+              <div key={index} className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px] shadow-2xl">
                 <Image
                   alt="Product Image"
                   width={327}
@@ -28,7 +30,13 @@ const Project = ({ }) => {
                     {/* <span>description : {item_data.description}</span> */}
                     <span>$ {item.price}.00</span>
                   </div>
-                  <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
+                  <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">
+                    <Link 
+                      href={`/`}
+                    >
+                    </Link>
+                    +
+                    </div>
                 </div>
               </div>
             ))
