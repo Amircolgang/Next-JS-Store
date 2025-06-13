a1484af9de884b390f392041068834653d3e2b3f
amircolgang <parham38385747@gmail.com>
2025-06-13 16:14:43 +0330

    Auto commit: changes on 2025-06-13 16:14:43

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,20 @@
 const http = require("node:http")
-// const fs = require("fs")
+const fs = require("fs")
 // const url = require("url")
 // const db = require("./db.json")
 
-let port = 8080
+let port = 3030
 let hostname = 'amirShirazi.ir'
 const server = http.createServer((req , res ) => {
 
-    if(req.method == "GET" && req.url.startsWith('/api')){
-        res.end("ok")
+    if(req.method == "GET" && req.url.startsWith('/api/project')){
+        fs.readFile("db.json" , (err) => {
+            if(err){
+                throw err
+            }
+
+            res.w
+        })
     }
 })
 
