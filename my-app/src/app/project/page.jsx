0f0b043fcdf723303f482897045667cb5ea8982a
amircolgang <parham38385747@gmail.com>
2025-06-13 16:44:26 +0330

    Auto commit: changes on 2025-06-13 16:44:26

--- my-app/src/app/project/page.jsx
+++ my-app/src/app/project/page.jsx
@@ -1,35 +1,39 @@
 import Image from "next/image"
 let data = ""
-fetch('http://localhost:3030/api/project')
-.then(res => res.json())
-.then(db => data = db )
- 
+let db = await fetch('http://localhost:3030/api/project')
+const json = await db.json();
+
 
 const Project = ({ }) => {
   // const item_data = item_dataFech()
-  console.log(data)
+  let item_data = json
   return (
     <>
-    <div>
-      <div className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px]">
-        <Image
-          alt="Product Image"
-          width={327}
-          height={142}
-          src={'/images/next.svg'}
-          className="rounded-2xl"
-        />
-        <div className="w-[327px] h-[106px] flex justify-between">
-          <div className="flex flex-col justify-between">
-            {/* <span>Name : {item_data.title}</span> */}
-            {/* <span>Id : {item_data.id}</span> */}
-            {/* <span>description : {item_data.description}</span> */}
-            {/* <span>$ {item_data.price}.00</span> */}
-          </div>
-          <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
-        </div>
+      <div>
+        {
+          item_data.map(item => (
+            <div className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px] shadow-2xl">
+              <Image
+                alt="Product Image"
+                width={327}
+                height={142}
+                src={'/images/next.svg'}
+                className="rounded-2xl"
+              />
+              <div className="w-[327px] h-[106px] flex justify-between">
+                <div className="flex flex-col justify-between">
+                  <span>Name : {item.title}</span>
+                  <span>Id : {item.id}</span>
+                  {/* <span>description : {item_data.description}</span> */}
+                  <span>$ {item.price}.00</span>
+                </div>
+                <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
+              </div>
+            </div>
+          ))
+        }
+
       </div>
-    </div>
     </>
   )
 }
