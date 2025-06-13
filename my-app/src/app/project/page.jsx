607ae97e5405428c554130bb38c76f83e7e37d4c
amircolgang <parham38385747@gmail.com>
2025-06-13 16:23:13 +0330

    Auto commit: changes on 2025-06-13 16:23:13

---
+++ my-app/src/app/project/page.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+const Project = ({ }) => {
+  return (
+    <>
+      <div className="w-[375px] h-fit rounded-[25px] bg-[#ffff] p-[24] flex flex-col gap-[29px]">
+        <Image
+          alt="Product Image"
+          width={327}
+          height={142}
+          src={'/images/next.svg'}
+          className="rounded-2xl"
+        />
+        <div className="w-[327px] h-[106px] flex justify-between">
+          <div className="flex flex-col justify-between">
+            <span>Name : {item_data.category.name}</span>
+            <span>Id : {item_data.price}</span>
+            {/* <span>description : {item_data.description}</span> */}
+            <span>$ {item_data.price}.00</span>
+          </div>
+          <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">+</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Project
