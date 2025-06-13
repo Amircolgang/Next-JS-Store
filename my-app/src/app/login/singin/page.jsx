cd25d89e6c1ca4e6c8bb331477f7d52ae7a77f64
amircolgang <parham38385747@gmail.com>
2025-06-13 17:05:45 +0330

    Auto commit: changes on 2025-06-13 17:05:44

--- my-app/src/app/login/singin/page.jsx
+++ my-app/src/app/login/singin/page.jsx
@@ -1,11 +1,58 @@
-const SingIn = ({}) => {
+const SingIn = ({ }) => {
   return (
     <>
+    <div>
+      <div>
         <div>
-            <div>
-              
-            </div>
+          <div>
+            <span>
+              Welcome back
+            </span>
+            <span>
+              Welcome back! Please enter your details.
+            </span>
+          </div>
         </div>
+        <div>
+          <div>
+            <span>
+              Email
+            </span>
+            <input type="text" placeholder="Enter your email" />
+          </div>
+          <div>
+            <span>
+              Password
+            </span>
+            <input type="text" placeholder="**********" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <input type="checkbox" name="" id="" />
+              <span>Remember me</span>
+            </div>
+            <div>
+              Forgot password
+            </div>
+          </div>
+          <div>
+            <button></button>
+            <div>
+              <span>
+                Sign in with Google
+              </span>
+            </div>
+          </div>
+          <div>
+            <span>Don’t have an account? </span>
+            <span>Sign up fo free!</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
     </>
   )
 }
