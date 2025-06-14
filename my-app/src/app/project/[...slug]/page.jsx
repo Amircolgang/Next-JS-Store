6eb95c4d2b6c8a147cf515076dd9102c934707e5
amircolgang <parham38385747@gmail.com>
2025-06-14 12:21:16 +0330

    Auto commit: changes on 2025-06-14 12:21:15

--- my-app/src/app/project/[...slug]/page.jsx
+++ my-app/src/app/project/[...slug]/page.jsx
@@ -1,6 +1,11 @@
+let db = await fetch('http://localhost:3030/api/project')
+const json = await db.json();
+
 const SlugerPrudoct = ({ params }) => {
     let {slug} = params
     console.log(slug)
+    let categor = json[0][slug]
+    console.log(categor)
     return (
     <>
         {
