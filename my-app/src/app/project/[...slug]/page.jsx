984d85e18b59c95d059153f108033b4ebbaac04f
amircolgang <parham38385747@gmail.com>
2025-06-14 12:25:30 +0330

    Auto commit: changes on 2025-06-14 12:25:29

--- my-app/src/app/project/[...slug]/page.jsx
+++ my-app/src/app/project/[...slug]/page.jsx
@@ -3,13 +3,13 @@ const json = await db.json();
 
 const SlugerPrudoct = ({ params }) => {
     let {slug} = params
-    console.log(slug)
-    let categor = json[0][slug]
-    console.log(categor)
+    // console.log(slug)
+    let categor = json[0].category
+    
     return (
     <>
         {
-            
+            categor.map((item) => item )        
         }
     </>
   )
