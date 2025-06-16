c17643ece16c196b2e26ce791c880740ae9d5a43
amircolgang <parham38385747@gmail.com>
2025-06-16 12:14:11 +0330

    Amircolgang: changes on 2025-06-16 12:14:10

--- my-app/src/app/project/[...slug]/page.jsx
+++ my-app/src/app/project/[...slug]/page.jsx
@@ -3,12 +3,12 @@ const json = await db.json();
 
 const SlugerPrudoct = ({ params }) => {
     let {slug} = params
-    console.log(slug[0])
+    let itemID = slug[0]
     let data = json
     let categoryItem = data.category
     // let data_item = json.find(item => item[0])       
     // categoryItem.map(item => console.log(Object.keys(item)))
-    console.log(categoryItem.slug)
+    console.log(categoryItem.itemID)
     return (
       <>
       {
