d3e139c2bba503f53c0272fe05a6f3490e018960
amircolgang <parham38385747@gmail.com>
2025-06-16 15:16:31 +0330

    Amircolgang: changes on 2025-06-16 15:16:26

--- my-app/src/app/project/[...slug]/page.jsx
+++ my-app/src/app/project/[...slug]/page.jsx
@@ -1,14 +1,14 @@
 let db = await fetch('http://localhost:3000/api/project')
 const json = await db.json();
 
-const SlugerPrudoct = ({ params }) => {
+const SlugerPrudoct = async ({ params }) => {
     let {slug} = params
     let itemID = slug[0]
     let data = json
     let categoryItem = data.category
     // let data_item = json.find(item => item[0])       
     // categoryItem.map(item => console.log(Object.keys(item)))
-    let product = categoryItem[itemID]
+    let product = await categoryItem[itemID]
     console.log(product)
     return (
       <>
