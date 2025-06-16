5297a674cf8fa2878b25f0e990cd621deaca9f08
amircolgang <parham38385747@gmail.com>
2025-06-16 11:44:33 +0330

    Amircolgang: changes on 2025-06-16 11:44:33

--- my-app/src/app/project/page.jsx
+++ my-app/src/app/project/page.jsx
@@ -6,16 +6,15 @@ const json = await db.json();
 import Header from "@/components/header";
 
 const Project = ({ }) => {
-  let titleSesionLearn  = json.category
-  console.log(titleSesionLearn)
+  // console.log(titleSesionLearn)
   // const item_data = item_dataFech()
-  // let objectKeyinProject = []
-  // let titleSesionLearn = []
-  // let item_data = json
-  // let category = item_data[0].category
-  // category.map(item => objectKeyinProject.push(Object.keys(item)))
-  // objectKeyinProject.map((object => titleSesionLearn.push({name : object[0]})))
-  // console.log(titleSesionLearn.map(item => console.log(item.name)))
+  let objectKeyinProject = []
+  let titleSesionLearn = []
+  let item_data = json
+  let category = item_data[0].category
+  category.map(item => objectKeyinProject.push(Object.keys(item)))
+  objectKeyinProject.map((object => titleSesionLearn.push({name : object[0]})))
+  console.log(titleSesionLearn.map(item => console.log(item.name)))
   return (
     <>
       <Header />
