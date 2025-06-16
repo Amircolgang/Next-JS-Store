bed0c272ef8d604a76a90d8c857bceb7972ede0c
amircolgang <parham38385747@gmail.com>
2025-06-16 11:25:26 +0330

    Amircolgang: changes on 2025-06-16 11:25:25

--- my-app/src/app/api/project/route.js
+++ my-app/src/app/api/project/route.js
@@ -1,8 +1,113 @@
-export default async function GET(){
-    
-    try {
+export default async function GET() {
+    let category = [
+        {
+            "front-end": [
+                {
+                    "id": 1,
+                    "title": "nodeJS",
+                    "price": 25000
+                },
+                {
+                    "id": 2,
+                    "title": "NustJS",
+                    "price": 60000
+                },
+                {
+                    "id": 3,
+                    "title": "NextJS",
+                    "price": 1250000
+                },
+                {
+                    "id": 4,
+                    "title": "VanilaJS",
+                    "price": 25000
+                },
+                {
+                    "id": 5,
+                    "title": "PHP",
+                    "price": 60000
+                },
+                {
+                    "id": 6,
+                    "title": "Laravel",
+                    "price": 1250000
+                }
+            ]
+        },
+        {
+            "back-end": [
+                {
+                    "id": 1,
+                    "title": "nodeJS",
+                    "price": 25000
+                },
+                {
+                    "id": 2,
+                    "title": "NustJS",
+                    "price": 60000
+                },
+                {
+                    "id": 3,
+                    "title": "NextJS",
+                    "price": 1250000
+                },
+                {
+                    "id": 4,
+                    "title": "VanilaJS",
+                    "price": 25000
+                },
+                {
+                    "id": 5,
+                    "title": "PHP",
+                    "price": 60000
+                },
+                {
+                    "id": 6,
+                    "title": "Laravel",
+                    "price": 1250000
+                }
+            ]
+        },
+        {
+            "hack": [
+                {
+                    "id": 1,
+                    "title": "nodeJS",
+                    "price": 25000
+                },
+                {
+                    "id": 2,
+                    "title": "NustJS",
+                    "price": 60000
+                },
+                {
+                    "id": 3,
+                    "title": "NextJS",
+                    "price": 1250000
+                },
+                {
+                    "id": 4,
+                    "title": "VanilaJS",
+                    "price": 25000
+                },
+                {
+                    "id": 5,
+                    "title": "PHP",
+                    "price": 60000
+                },
+                {
+                    "id": 6,
+                    "title": "Laravel",
+                    "price": 1250000
+                }
+            ]
+        }
+    ]
+}
+try {
+    return new Response(
         
-    } catch (error) {
-        
-    }
+    )
+} catch (error) {
+
 }
