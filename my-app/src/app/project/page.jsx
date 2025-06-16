353667e472954bb8dd26eb2c41d05140a5951178
amircolgang <parham38385747@gmail.com>
2025-06-16 11:42:23 +0330

    Amircolgang: changes on 2025-06-16 11:42:22

--- my-app/src/app/project/page.jsx
+++ my-app/src/app/project/page.jsx
@@ -33,7 +33,7 @@ const Project = ({ }) => {
                 />
                 <div className="w-[327px] h-[106px] flex justify-between">
                   <div className="flex flex-col justify-between">
-                    <span>Name : {item.name}</span>
+                    <span>Name : {item[0][item].name}</span>
                     {/* <span>description : {item_data.description}</span> */}
                   </div>
                   <div className="w-[40px] cursor-pointer h-[39px] rounded-[25px] bg-[#1A1A1A] text-amber-200 text-4xl flex items-center justify-center">
